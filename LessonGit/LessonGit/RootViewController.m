6caa59fbd38437f880329aa1f57c8faa5b72e9fa
gityoung <gityoung@163.com>
2015-04-15 16:53:21 +0800

    2

--- LessonGit/LessonGit/RootViewController.m
+++ LessonGit/LessonGit/RootViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"hello";
+    self.view.backgroundColor = [UIColor whiteColor];
 }
 
 - (void)didReceiveMemoryWarning {
