1eaf5da48710755c613eba84736a1b351e2f3e21
gityoung <gityoung@163.com>
2015-04-15 16:35:43 +0800

    helloworld

---
+++ LessonGit/LessonGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LessonGit
+//
+//  Created by young on 15/4/15.
+//  Copyright (c) 2015年 young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LessonGit/LessonGit/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  LessonGit
+//
+//  Created by young on 15/4/15.
+//  Copyright (c) 2015年 young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
